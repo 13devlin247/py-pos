dfcc457d3e84d8bb0b76a74bee48a7465d04e45c
zhijian.tai <zhijian.tai@gmail.com>
2011-05-24 09:39:00 +0000

    append profit count function

--- kernal/models.py
+++ kernal/models.py
@@ -130,6 +130,7 @@ class Bill(models.Model):
     discount = models.DecimalField(max_digits=100,  decimal_places=2)
     total_price = models.DecimalField(max_digits=100,  decimal_places=2)
     tendered_amount = models.DecimalField(max_digits=100,  decimal_places=2)
+    profit = models.DecimalField(max_digits=100,  decimal_places=2)
     change = models.DecimalField(max_digits=100,  decimal_places=2)
     customer = models.ForeignKey(Customer)
     create_at = models.DateTimeField(auto_now_add = True)
