ad9c07a989979065dc670dd1678eb8582d3962ae
zhijian.tai <zhijian.tai@28eb719f-8955-6c0a-6b0d-9a18842fd0ac>
2011-08-07 23:49:54 +0000

    increase BarnOwl function

--- branches/refactory-1.0/kernal/testKernal.py
+++ branches/refactory-1.0/kernal/testKernal.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import unittest
+import datetime
 p1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 p2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 
@@ -12,7 +13,8 @@ sys.stdout = sys.stderr
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pos.settings' 
 import logging
 from barn import BarnMouse, BarnOwl
-from pos.kernal.models import Product, Category, Brand, Type, UOM, InStockRecord
+from pos.kernal.models import Product, Category, Brand, Type, UOM, InStockRecord,\
+    Bill, StockCost, Counter
 
 logging.basicConfig(
     level = logging.WARN,
@@ -48,6 +50,24 @@ class TestBarnMouse(unittest.TestCase):
         mouse.InStock(inStockBatch, 3, 3, owl.purchase, None)
         assert InStockRecord.objects.filter(inStockBatch = inStockBatch).count() > 0 
         
+    def test_outstock(self):
+        mouse = BarnMouse(self.products[0])
+        bill = Bill()
+        bill.subtotal_price = 100
+        bill.discount = 0
+        bill.total_price = 100
+        bill.tendered_amount = 100
+        bill.profit = 50
+        bill.change = 0
+        bill.create_at = datetime.datetime.today()
+        bill.counter = Counter()
+        bill.pk = 65535
+        bill.mode = "x"
+                
+        mouse.OutStock(bill, 1, 1000, "Cash Sales", "serial-2")
+        stockcost = StockCost.objects.get(product = self.products[0]) 
+        assert stockcost.qty == 5 
+        
     def test_cost_by_product__(self):
         logger.debug("Test Product Cost")
         mouse = BarnMouse(self.products[0])
@@ -59,12 +79,14 @@ class TestBarnMouse(unittest.TestCase):
         logger.debug("Test DELETE Product instock record")
         mouse = BarnMouse(self.products[0])
         assert mouse.QTY() == 6
+        assert mouse.Cost() == 1750
         owl = BarnOwl()
+        
         inStockRecords = owl.InStock(owl.purchase, self._build_input_dict())
         for inStockRecord in inStockRecords:
             mouse.Delete("ForTestOnly", InStockRecord, inStockRecord.pk)
-        assert mouse.QTY() == 3
-        
+        assert mouse.QTY() == 6
+        assert mouse.Cost() == 1750
     
     def test_update_cost__(self):
         logger.debug("Test UPDATE Product Cost")
@@ -213,7 +235,8 @@ class TestBarnMouse(unittest.TestCase):
         inputDict[u'65536'] [u'serial-0']= u'NKPPL001'    
         inputDict[u'65536'] [u'cost'] = u''    
         inputDict[u'65536'] [u'quantity'] = u'3'                    
-        return inputDict        
+        return inputDict
+            
 class TestBarnOwl(unittest.TestCase):    
     def setUp(self):
         logger.debug("TestBarnOwl.Setup")
