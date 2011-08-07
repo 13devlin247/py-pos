ecc5d331266fc8c66efb5679c47bac650bfeafbe
zhijian.tai <zhijian.tai@28eb719f-8955-6c0a-6b0d-9a18842fd0ac>
2011-08-07 23:49:54 +0000

    increase BarnOwl function

--- kernal/barn.py
+++ kernal/barn.py
@@ -1,7 +1,9 @@
 from datetime import date, datetime
 from django.contrib.auth.models import User
-from pos.kernal.models import InStockRecord, OutStockRecord, StockCost, Product, Supplier, Customer, InStockBatch, SerialNo
+from pos.kernal.models import InStockRecord, OutStockRecord, StockCost, Product, Supplier, Customer, InStockBatch, SerialNo,\
+    Bill, Payment, Category, Brand, UOM, ConsignmentOutDetail, Counter
 import logging
+
 
 
 logging.basicConfig(
@@ -17,7 +19,14 @@ class BarnMouse:
         logger.debug("BarnMose '%s' build", product.name)
         self.product = product
         self.is_serialable = self._check_serial()
-     
+        self.foc_product = self._check_foc_product()
+
+    def _check_foc_product(self):
+        if "-foc-product" in self.product.name:
+            logger.debug("'%s' is FOC Product", self.product.name)
+            return True
+        return False
+        
     def _check_serial(self):
         serials = SerialNo.objects.filter(inStockRecord__product = self.product)
         if serials.count() > 0:
@@ -44,8 +53,8 @@ class BarnMouse:
     def __count_inventory_stock__(self, starttime, endtime, product):
         result = []
 
-        inStockRecords = InStockRecord.objects.filter(product = product)
-        outStockRecords = OutStockRecord.objects.filter(product = product)
+        inStockRecords = InStockRecord.objects.filter(product = product).filter(active = True)
+        outStockRecords = OutStockRecord.objects.filter(product = product).filter(active = True)
         inStockSummary = self.__count_product_stock__(starttime, endtime, inStockRecords, product)
         outStockSummary = self.__count_product_stock__(starttime, endtime, outStockRecords, product)
         # count old stock record
@@ -115,7 +124,9 @@ class BarnMouse:
         inventory_summary = self.__count_inventory_stock__(starttime, endtime, self.product)
         qty = inventory_summary[5]
         cost = inventory_summary[6]
-        avg_cost = cost / qty
+        avg_cost = 0
+        if qty != 0:
+            avg_cost = cost / qty
         on_hand_value = qty * avg_cost
         logger.debug("Product: '%s' avarage cost: '%s'",self.product.name, avg_cost)
         
@@ -134,6 +145,38 @@ class BarnMouse:
         stockCost.instock_create_at = self._query_last_record_datetime(InStockRecord)
         stockCost.outstock_create_at = self._query_last_record_datetime(OutStockRecord)
         stockCost.save()
+        
+    def __lock_serial_no__(self, imei):
+        serial_no = None
+        try:
+            serial_no = SerialNo.objects.get(serial_no = imei)
+            serial_no.active = False
+            serial_no.save()
+            logger.debug("product found by imei : %s ", imei)
+        except SerialNo.DoesNotExist:
+            logger.debug("no imei no '%s'. found", imei)
+        return serial_no    
+    
+    def OutStock(self, bill, qty, price, reason, serials):
+        outStockRecord = OutStockRecord()
+        outStockRecord.bill = bill
+        outStockRecord.product = self.product
+        serial_no = self.__lock_serial_no__(serials)
+        outStockRecord.serial_no = serial_no
+        outStockRecord.unit_sell_price = price
+        outStockRecord.quantity = qty
+        outStockRecord.amount = price * qty 
+        outStockRecord.sell_index = 0;
+        outStockRecord.profit = 0;
+        outStockRecord.cost = -1;
+        outStockRecord.type = type
+        outStockRecord.save()
+        logger.info("Product: '%s' OutStockRecord build: '%s' , bill pk: '%s', qty: '%s', price: '%s', reason: '%s', serials: '%s' ", self.product.name, outStockRecord.pk, bill.pk, qty, price, reason, serials)
+        
+        stockCost = StockCost.objects.get(product = self.product)
+        stockCost.qty = stockCost.qty - qty 
+        stockCost.save()
+        return outStockRecord
     
     def InStock(self, inStockBatch, qty, cost, reason, serials):
         if cost == "":
@@ -145,6 +188,7 @@ class BarnMouse:
         inStockRecord.cost = cost
         inStockRecord.quantity = qty
         inStockRecord.status = reason
+        inStockRecord.active = True
         inStockRecord.save()
         
         if self.is_serialable:
@@ -186,7 +230,6 @@ class BarnMouse:
     
     def QTY(self):
         try:
-            logger.debug("look up Product: '%s' QTY", self.product.pk)
             query = StockCost.objects.get(product=self.product)
             qty = query.qty
         except StockCost.DoesNotExist:
@@ -207,14 +250,30 @@ class BarnMouse:
             logger.warn("instance '%s' , Cost: '%s' does NOT update correctly", pk, cost)
     
     def Delete(self, reason, Models, pk):
-        
-        pass
-    
-    
+        try:
+            logger.debug("Delete '%s', pk:'%s', reason:'%s'", Models, pk, reason)
+            model = Models.objects.get(pk = pk)
+            model.active = False
+            model.save()
+            if Models == InStockRecord:
+                logger.debug("InStockRecord '%s' has been delete, recalc cost", pk)
+                self._recalc_cost()
+        except model.DoesNotExist:
+            logger.error("Delete '%s', pk:'%', reason:'%s' Fail, Does Not Exist",Models, pk, reason)
 
 class BarnOwl:
     def __init__(self):
+        # instock
         self.purchase = "purchase"
+        
+        # outstock
+        self.cash = "Cash Sales"
+        self.invoice =  "Invoice"
+        self.adjust = "adjust"
+        self.consignment = "Consignment"
+        self.Consignment_in_balance = "Consignment_in_balance"
+        self.Consignment_out_sales = "Consignment_out_sales"
+
     """
     output 
     {
@@ -241,6 +300,97 @@ class BarnOwl:
                 serials.append(productDict[item])
         logger.debug("serials count: '%s' ", len(serials))
         return serials
+
+    def __build_FOC_product__(self, product_name):    
+        category = None
+        brand = None
+        uom = None
+        categorys = Category.objects.filter(category_name = "FOC")
+        if categorys.count() == 0:
+            category = Category()
+            category.category_name = "FOC"
+            category.save()
+        else:
+            category = categorys[0]
+        brands = Brand.objects.filter(brand_name = "FOC")
+        if brands.count() == 0:
+            brand = Brand()
+            brand.category = category
+            brand.brand_name = "FOC"
+            brand.save()
+        else:
+            brand = brands[0]
+        uoms = UOM.objects.filter(name="FOC")
+        if uoms.count() == 0:
+            uom = UOM()
+            uom.name = "FOC"
+            uom.save()
+        else:
+            uom = uoms[0]        
+        product = Product()
+        product.barcode = "FOC"
+        product.name = product_name
+        product.description = product_name
+        product.category =  category
+        product.brand =  brand
+        product.retail_price = 0
+        product.cost = 0
+        product.uom = uom
+        product.active = False
+        product.save()
+        return product    
+
+    def _query_product(self, product_key):
+        product = None
+        if "-foc-product" in product_key:
+            logger.debug("Foc product !! : %s ", product_key)    
+            products = Product.objects.filter(name=product_key)
+            if products.count() == 0:
+                logger.debug("Foc product NOT found in DB !! : %s , create it", product_key)    
+                product =  self.__build_FOC_product__(product_key)
+            else:
+                product = products[0]
+        else:
+            serial_no = None
+            try:
+                serial_no = SerialNo.objects.get(serial_no = product_key)
+                product = serial_no.product
+                logger.debug("product found by imei : %s ", product_key)
+            except SerialNo.DoesNotExist:
+                logger.debug("no imei no '%s'. found", product_key)
+                product = Product.objects.get(pk = product_key)
+        return product
+    
+    def __build_outstock_record__(self, request, bill, payment, dict , type):
+        outStockRecords = []
+        # build OutStockRecord to save data
+        for barcode in dict:
+            logger.debug("build OutStockRecord by: '%s'", barcode)
+            product = self._query_product(barcode)
+            bill = bill
+            qty = dict[barcode]['quantity'] [0]
+            unit_sell_price = dict[barcode]['price'][0]
+            reason = type
+            isSerial = self._is_serial_no(barcode)
+            serial = None
+            if isSerial:
+                serial = SerialNo.objects.get(serial_no = serial)
+            mouse = BarnMouse(product)
+            outStockRecord = mouse.OutStock(bill, qty, unit_sell_price, reason, serial)
+            outStockRecords.append(outStockRecord)
+            """
+            if payment.type == "Consignment":
+                consignmentOut = ConsignmentOutDetail()
+                consignmentOut.payment = payment
+                consignmentOut.outStockRecord = outStockRecord
+                consignmentOut.serialNo = serial
+                consignmentOut.quantity = outStockRecord.quantity
+                consignmentOut.balance = 0
+                consignmentOut.save()
+                logger.debug("build Prodict '%s' OutStockRecord '%s' consignment detail.", outStockRecord.product.name, outStockRecord.pk )
+            """                
+            return outStockRecords
+    
     
     def __build_instock_records__(self, inStockBatch, inventoryDict, reason):
         logger.debug("build InStock records")
@@ -264,7 +414,145 @@ class BarnOwl:
             inStockRecords.append(inStockRecord)
             logger.debug("filter serial no by product: '%s'",  product.name)
         return inStockRecords
+    
+    def __query_customer__(self, customerName):
+        customerList = Customer.objects.filter(name=customerName)
+        customer = None
+        if customerList.count() == 0:
+            logger.debug("Customer '%s' not found, auto create cusomer info.")
+            customer = Customer()
+            customer.customer_code = customerName
+            customer.name = customerName
+            customer.save()
+            logger.debug("Customer build success: %s", customer)
+        customer = Customer.objects.filter(name=customerName)[0]
+        logger.debug("Customer found: %s", customer)
+        return customer    
+
+    def __build_bill__(self, dict, customer, counter):
+        bill = Bill()
+        bill.mode = dict.get('mode', 'sale')
+        bill.subtotal_price = dict.get('subTotal', '0')
+        bill.discount = dict.get('discount', '0')
+        bill.total_price = dict.get('total', '0')
+        bill.tendered_amount = dict.get('amountTendered', '0')
+        bill.change = dict.get('change', '0')
+        bill.customer = customer
+        bill.profit = 0
+        bill.counter = counter
+        bill.sales_by = User.objects.get(pk=int(dict.get('salesby','-1')))
+        bill.issue_by = User.objects.get(pk=dict.get('_auth_user_id'))
+        bill.fulfill_payment = False
+        bill.save()
+        logging.info("Bill '%s' create, customer: '%s', counter: '%s '", bill.pk, customer, counter)
+        return bill
+    
+    def __build_payment__(self, dict, bill, customer):    
+        payment = Payment()
+        payment.bill = bill
+        salesMode = dict.get('salesMode', '')
+        logger.debug("SalesMode: %s", salesMode)
+        if salesMode == "cash":
+            payment.term = "Cash"
+            payment.type = "Cash Sales"
+            payment.status = "Complete"
+        elif salesMode == "invoice":
+            payment.term = customer.term
+            payment.type = "Invoice"
+            payment.status = "Incomplete"    
+        elif salesMode == "adjust":
+            payment.term = "adjust"
+            payment.type = "adjust"
+            payment.status = "Complete"            
+        elif salesMode == "Consignment":
+            payment.term = "Consignment"
+            payment.type = "Consignment"
+            payment.status = "Incomplete"    
+        elif salesMode == "Consignment_in_balance":
+            payment.term = "Consignment_in_balance"
+            payment.type = "Consignment_in_balance"
+            payment.status = "Incomplete"    
+        elif salesMode == "Consignment_out_sales":
+            payment.term = "Consignment_out_sales"
+            payment.type = "Consignment_out_sales"
+            payment.status = "Complete"         
+        else:
+            logger.error("salesMode out of expect: %s ", salesMode)
+        logger.debug("build '%s' payment", salesMode)                
+        transactionNo = dict.get('transactionNo', '')
+        if transactionNo != '': 
+            logger.info("paid by creadit card")
+            payment.term = "CreaditCard"
+            payment.transaction_no = transactionNo
+        logger.debug("payment success builded")
+        payment.save()    
+        return payment    
+    
+    """
+        __convert_sales_URL_2_dict__(): 
         
+        Input Data array
+            [
+            // Bill data
+             (u'customer', [u'Cash']), 
+             (u'salesby', [u'1']), 
+             (u'amountTendered', [u'300']), 
+             (u'salesMode', [u'cash']), 
+             (u'item', [u'EXT5001']), 
+             (u'mode', [u'sale']), 
+             (u'discount', [u'0.00']), 
+             (u'total', [u'300']), 
+             (u'subTotal', [u'300']), 
+             (u'change', [u'0'])
+            // Payment data
+             (u'transactionNo', [u'']), 
+            // OutStockRecord data
+             (u'EXT5001_pk', [u'35']), 
+             (u'EXT5001_quantity', [u'1']), 
+             (u'EXT5001_imei', [u'EXT5001']), 
+             (u'EXT5001_price', [u'300']), 
+            ]
+            
+        Output Data Dict
+            {
+             u'EXT5001': {
+                             u'imei': [u'EXT5001'], 
+                             u'pk': [u'35'], 
+                             u'price': [u'300'], 
+                             u'quantity': [u'1']
+                         }
+             }    
+    """
+    def __convert_sales_URL_2_dict__(self, request):
+        salesDict = {}
+        sales_item = request.GET.lists()
+        logger.debug("instock items url parameters: %s", sales_item)    
+        for key,  value in sales_item:
+            if '_' not in key:
+                continue
+            pk = key.split("_")[0]
+            attr = key.split("_")[1]
+            if pk not in salesDict:
+                salesDict[pk] ={}
+            salesDict[pk] [attr]= value
+        return salesDict
+    
+    
+    def __build_bill_batch__(self, dict):
+        counter = None
+        counters = Counter.objects.filter(active=True)
+        if counters.count() != 0:
+            counter = counters.order_by('-create_at')[0]
+        else:
+            logger.warn("Can not found 'OPEN' Counter, direct to open page")
+            return False
+        # process Request parameter
+        customer_name = dict.get("customer")
+        customer = self.__query_customer__(customer_name)
+        bill = self.__build_bill__(dict, customer, counter)
+        payment = self.__build_payment__(dict, bill, customer)
+        return bill
+            
     def __build_instock_batch__(self, dict):
         mode = dict.get('mode', 'purchase')
         today = date.today()
@@ -313,60 +601,12 @@ class BarnOwl:
         inStockRecords = self.__build_instock_records__(inStockBatch, in_stock_batch_dict, reason)
         logger.debug("InStockBatch '%s' build", inStockBatch.pk)
         return inStockRecords
-        
-#    def __build_outstock_record__(self.request, bill, payment, dict , type):
-#        outStockRecords = []
-#        # build OutStockRecord to save data
-#        for barcode in dict:
-#            logger.debug("build OutStockRecord by: '%s'", barcode)
-#            outStockRecord = OutStockRecord()
-#            outStockRecord.bill = bill
-#            outStockRecord.barcode = barcode
-#            logger.info("looking for pk : %s " % barcode)
-#            if "-foc-product" in barcode:
-#                logger.info("Foc product !! : %s " % barcode)    
-#                products = Product.objects.filter(name=barcode)
-#                if products.count() == 0:
-#                    logger.info("Foc product NOT found in DB !! : %s , create it" % barcode)    
-#                    outStockRecord.product =  __build_FOC_product__(barcode)
-#                else:
-#                    outStockRecord.product = products[0]
-#            else:
-#                serial_no = __lock_serial_no__(request, barcode)
-#                outStockRecord.serial_no = serial_no
-#                if serial_no:
-#                    logger.info("product found by imei : %s " % barcode)
-#                    outStockRecord.product = serial_no.inStockRecord.product
-#                else:
-#                    logger.info("product found by pk : %s " % barcode)
-#                    outStockRecord.product = Product.objects.get(pk=barcode)
-#            outStockRecord.unit_sell_price = dict[barcode]['price'][0]
-#            outStockRecord.quantity = dict[barcode]['quantity'] [0]
-#            outStockRecord.amount = str(float(dict[barcode]['price'][0]) * float(dict[barcode]['quantity'] [0])) 
-#            outStockRecord.sell_index = 0;
-#            outStockRecord.profit = 0;
-#            outStockRecord.cost = -1;
-#            outStockRecord.type = type
-#            outStockRecord.save()
-#            outStockRecords.append(OutStockRecord.objects.get(pk=outStockRecord.pk))
-#            if payment.type == "Consignment":
-#                consignmentOut = ConsignmentOutDetail()
-#                consignmentOut.payment = payment
-#                consignmentOut.outStockRecord = outStockRecord
-#                consignmentOut.serialNo = serial_no
-#                consignmentOut.quantity = outStockRecord.quantity
-#                consignmentOut.balance = 0
-#                consignmentOut.save()
-#                logger.debug("build Prodict '%s' OutStockRecord '%s' consignment detail.", outStockRecord.product.name, outStockRecord.pk )
-#            return outStockRecords
-#        
+    
     def OutStock(self, reason, out_stock_batch_dict):
         logger.debug("Reason: '%s', dict: %s", reason, out_stock_batch_dict)
-#        bill = self.__build_instock_batch__(out_stock_batch_dict)
-#        outStockRecords = self.__build_instock_records__(bill, out_stock_batch_dict, reason)
-#        logger.debug("InStockBatch '%s' build", outStockRecords.pk)
-#        return outStockRecords
-        pass
+        bill = self.__build_bill_batch__(out_stock_batch_dict)
+        outStockRecords = self.__build_instock_records__(bill, out_stock_batch_dict, reason)
+        return outStockRecords
         
     def Cost(self, reason, out_stock_batch_dict):
         return False
@@ -384,3 +624,4 @@ class Hermes:
     def Profit(self, product):
         return False
     
+
