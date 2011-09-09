e7a136fcdfca3b38b449a973aa8930d59ca9f66c
zhijian.tai <zhijian.tai@28eb719f-8955-6c0a-6b0d-9a18842fd0ac>
2011-09-09 05:13:14 +0000

    fix javascript Float problem

--- kernal/views.py
+++ kernal/views.py
@@ -1092,7 +1092,7 @@ def DepositList(request):
     
     querySet = __search__(Deposit, Q(customer__name__contains=keyword)|Q(refBill=keyword)|Q(pk=_str_2_int(keyword)))
     list = __autocomplete_wrapper__(querySet, lambda model: str(model.pk))    
-    return HttpResponse(list, mimetype="text/plain")
+    return HttpResponse(list.encode("utf-8"), mimetype="text/plain")
 
 def ServiceList(request):
     keyword = request.GET.get('q', "")
