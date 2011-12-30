f5684a385af0fce5a19fffb7a89514c3b623bc55
zhijian.tai@gmail.com <zhijian.tai@gmail.com@28eb719f-8955-6c0a-6b0d-9a18842fd0ac>
2011-12-30 01:46:48 +0000

    branch for clothes shop,

---
+++ branches/clothes/kernal/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from pos.kernal.models import Product, ProductAdmin
+from pos.kernal.models import InStockRecord, InStockRecordForm
+from pos.kernal.models import OutStockRecord, OutStockRecordForm
+from pos.kernal.models import Bill, BillForm, BillAdmin
+from pos.kernal.models import Payment, PaymentAdmin
+from pos.kernal.models import Supplier, SupplierAdmin
+from pos.kernal.models import Customer, CustomerAdmin
+from pos.kernal.models import Category, CategoryForm
+from pos.kernal.models import Brand, BrandForm
+from pos.kernal.models import UOM
+from pos.kernal.models import InStockBatch
+from pos.kernal.models import SerialNo
+from pos.kernal.models import Category
+from pos.kernal.models import Brand
+from pos.kernal.models import Type, TypeForm
+from pos.kernal.models import Counter
+from pos.kernal.models import CounterAdmin
+from pos.kernal.models import Company
+from pos.kernal.models import Algo
+from pos.kernal.models import Deposit
+from scheduling.models import ClothesTemplate, Worker, WorkerAbility, Task,\
+    ClothesChoosed
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(InStockRecord)
+admin.site.register(InStockBatch)
+admin.site.register(OutStockRecord)
+admin.site.register(Bill, BillAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(UOM)
+admin.site.register(SerialNo)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Type)
+admin.site.register(Counter, CounterAdmin)
+admin.site.register(Company)
+admin.site.register(Algo)
+admin.site.register(Deposit)
+
+admin.site.register(ClothesTemplate)
+admin.site.register(Worker)
+admin.site.register(WorkerAbility)
+admin.site.register(Task)
+admin.site.register(ClothesChoosed)
+
+
+
+
