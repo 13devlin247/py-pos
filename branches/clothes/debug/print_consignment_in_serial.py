b83dce0e4140ebdba42c3eb7e8d539a4c1777802
zhijian.tai@gmail.com <zhijian.tai@gmail.com@28eb719f-8955-6c0a-6b0d-9a18842fd0ac>
2012-01-05 05:48:40 +0000

    add debug tool for print consignmeng_in_stock

---
+++ branches/clothes/debug/print_consignment_in_serial.py
@@ -0,0 +1,32 @@
+import sys, os  
+p1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+p2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+sys.path.append(p1)
+sys.path.append(p2)
+sys.stdout = sys.stderr
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pos.settings' 
+from django.db.models import Q
+from pos.kernal.barn import *
+import django
+from pos.kernal.models import *
+import re
+
+import sys    
+        
+if __name__ == "__main__":
+    inStockRecords = InStockRecord.objects.filter(active=True).filter(type="Consignment_IN")
+    stock = {}
+    for inStockRecord in inStockRecords:
+        stock[inStockRecord] = None
+    
+    outstocks = OutStockRecord.objects.filter(active=True)
+    for outstock in outstocks:
+        if outstock.inStockRecord in stock:
+            del stock[outstock.inStockRecord]
+            print "Consignment record %s out" % outstock.inStockRecord.pk
+    print "======================"
+    
+    for instock in stock:
+        serials = SerialNo.objects.filter(inStockRecord = instock)
+        for serial in serials:
+            print serial.serial_no
